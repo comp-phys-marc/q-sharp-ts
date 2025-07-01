bb75baedc135253bd5a4d84293966a2283761843
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-07-01 15:40:45 -0400

    Build

--- src/example.js
+++ src/example.js
@@ -3,7 +3,7 @@ import Parser from './parser.js';
 import * as fs from 'fs';
 let total = 0;
 // Replace me!
-const baseDir = '/Users/marcusedwards/Documents/School/Courses/CMPT_981/final-project/code';
+const baseDir = '..';
 // Try a few scripts!
 // const q_sharp = fs.readFileSync(baseDir + '/spec/q-sharp/demo.qs', 'utf8');  // Fully parses and compiles...
 // const q_sharp = fs.readFileSync(baseDir + '/spec/q-sharp/bell-states.qs', 'utf8');  // Fully parses and partially compiles...
