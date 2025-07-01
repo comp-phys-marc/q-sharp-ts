13689ca494b1a89259194922be02ab98e5598172
Marcus Edwards <marcusstephenedwards@gmail.com>
2025-07-01 15:37:33 -0400

    Build

--- src/main.js
+++ src/main.js
@@ -22,22 +22,4 @@ exports.parse = function (file) {
     return parseString(fs.readFileSync(file, 'utf8'));
 };
 exports.parseString = parseString;
-parseString(`
-operation ReflectAboutMarked(inputQubits : Qubit[]) : Unit {
-  use outputQubit = Qubit();
-  within {
-    // We initialize the outputQubit to (|0> = |1>) / sqrt(2), so that
-    // toggling it results in a (=1) phase.
-    X(outputQubit);
-    H(outputQubit);
-    // Flip the outputQubit for marked states.
-    // Here, we get the state with alternating 0s and 1s by using the X
-    // operation on every other qubit.
-    for q in inputQubits [...2...] {
-      X(q);
-    }
-  } apply {
-    Controlled X(inputQubits, outputQubit);
-  }
-}`);
 //# sourceMappingURL=main.js.map
